5fc97bbd1f16dec671a84175253578927eb9972d
Aswathy <aswathyk2805@gmail.com>
2023-05-20 21:50:28 +0530

    Robotics Project

---
+++ larson_scanners1/larson_scanners1.ino
@@ -0,0 +1,41 @@
+// C++ code
+//
+
+void setup()
+{
+  pinMode(7, OUTPUT);
+  pinMode(6, OUTPUT);
+  pinMode(5, OUTPUT);
+  pinMode(4, OUTPUT);
+  pinMode(3, OUTPUT);
+  pinMode(2, OUTPUT);
+}
+
+void loop()
+{
+  digitalWrite(7, HIGH);
+  digitalWrite(7, LOW);
+  delay(100); // Wait for 1000 millisecond(s)
+  
+  digitalWrite(6, HIGH);
+  digitalWrite(6, LOW);
+  delay(100); // Wait for 1000 millisecond(s)
+  
+  digitalWrite(5, HIGH);
+  digitalWrite(5, LOW);
+  delay(100); // Wait for 1000 millisecond(s)
+  
+  digitalWrite(4, HIGH);
+  digitalWrite(4, LOW);
+  delay(100); // Wait for 1000 millisecond(s)
+  
+  digitalWrite(3, HIGH);
+  digitalWrite(3, LOW);
+  delay(100); // Wait for 1000 millisecond(s)
+  
+  digitalWrite(2, HIGH);
+  digitalWrite(2, LOW);
+  delay(100); // Wait for 1000 millisecond(s)
+}
+  
+ 
